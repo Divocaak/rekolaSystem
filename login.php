f4a366f31f8a1b68584b11be853cb679882a4dba
Vojtěch Divoký <divokyvojta@gmail.com>
2021-05-02 14:47:35 +0200

    vstup do databáze rdy

--- login.php
+++ login.php
@@ -35,7 +35,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT id, password, username FROM users WHERE username = ?";
+        $sql = "SELECT id, password, username, fName, lName, moneyRate, isAdmin, isDriver FROM users WHERE username = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
@@ -52,7 +52,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){                    
                     // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $hashed_password, $username);
+                    mysqli_stmt_bind_result($stmt, $id, $hashed_password, $username, $fName, $lName, $moneyRate, $isAdmin, $isDriver);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session
@@ -62,6 +62,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["username"] = $username;
+                            $_SESSION["fName"] = $fName;
+                            $_SESSION["lName"] = $lName;
+                            $_SESSION["moneyRate"] = $moneyRate;
+                            $_SESSION["isAdmin"] = $isAdmin;
+                            $_SESSION["isDriver"] = $isDriver;
+
 
                             // Redirect user to welcome page
                             header("location: index.php");
