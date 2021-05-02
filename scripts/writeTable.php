95409c46ec83fd90cf32c41b705ecc3e4f7883aa
Vojtěch Divoký <divokyvojta@gmail.com>
2021-05-02 23:36:30 +0200

    tabulka rdy, rozpracováné sčítání časů a následné určení mzdy

--- scripts/writeTable.php
+++ scripts/writeTable.php
@@ -1,16 +1,28 @@
 <?php
 require_once "config.php";
 
+
 $return = "";
-$sql = "SELECT user_id, t_from, t_to, activity FROM inputs WHERE user_id=" . $_POST["user"] . ";";
+$sql = "SELECT inputs.user_id, inputs.t_from, inputs.t_to, inputs.activity, users.fName, users.lName,
+    activities.name FROM inputs INNER JOIN users ON inputs.user_id=users.id
+    INNER JOIN activities ON inputs.activity=activities.id
+    WHERE user_id=" . $_POST["table_user"] . " AND
+    MONTH(inputs.t_from)=" . intval($_POST["table_month"]) . " AND YEAR(inputs.t_from)=" . intval($_POST["table_year"]) . ";";
+
 if ($result = mysqli_query($link, $sql)) {
     if(mysqli_num_rows($result) > 0){
         while ($row = mysqli_fetch_row($result)) {
+            $fName = $row[4];
+            $lName = $row[5];
+
+            $interval = date_diff(date_create($row[1]), date_create($row[2]));
+            $times[] = $interval;
+
             $return .= '<tr>
-                    <th scope="row">' . $row [1]. '</th>
+                    <td>' . $row [1]. '</td>
                     <td>' . $row[2] . '</td>
-                    <td>' . $row[3] . '</td>
-                    <td>' . 'celkem' . '</td>
+                    <td>' . $row[6] . '</td>
+                    <td>' . $interval->format("<b>%H</b>h <b>%i</b>m") . '</td>
                 </tr>';
         }
     }
@@ -21,16 +33,30 @@ if ($result = mysqli_query($link, $sql)) {
 }
 mysqli_close($link);
 
-echo '<p>Tabulka za MĚSÍC ROK pracovníka JMÉNO PŘÍJMENÍ</p>
-<table class="table table-dark table-hover">
+if ($return != ""){
+    echo '<p>Tabulka za měsíc číslo <b>' . $_POST["table_month"] . '</b>
+    roku <b>' . $_POST["table_year"] . '</b> pracovníka <b>' . $lName . ' ' . $fName . '</b></p>
+    <table class="table table-hover">
     <thead>
-        <tr>
-            <th scope="col">Od</th>
-            <th scope="col">Do</th>
-            <th scope="col">Činnost</th>
-            <th scope="col">Celkem</th>
-        </tr>
+    <tr>
+    <th scope="col">Od</th>
+    <th scope="col">Do</th>
+    <th scope="col">Činnost</th>
+    <th scope="col">Celkem</th>
+    </tr>
     </thead>
     <tbody>' . $return . '</tbody>
+    <tfoot>
+    <tr>
+    <td scope="col"></td>
+    <td scope="col"></td>
+    <td scope="col"></td>
+    <td scope="col">' . $totaltime->format("<b>%H</b>h <b>%i</b>m") . '</td>
+    </tr>
+    </tfoot>
     </table>';
+}
+else{
+    echo "<p>Error, nebo špatně zadané údaje.</p>";
+}
 ?>
