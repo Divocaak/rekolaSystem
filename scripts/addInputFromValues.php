f74cc8809ad7311eb653fa4f86408946e7957cd6
Vojtěch Divoký <divokyvojta@gmail.com>
2021-06-16 09:27:25 +0200

    start hotfixu

--- scripts/addInputFromValues.php
+++ scripts/addInputFromValues.php
@@ -2,10 +2,16 @@
 session_start();
 require_once "config.php";
 
-$sql = "INSERT INTO inputs (user_id, t_from, t_to, activity, distance) 
-VALUES (" . $_SESSION["id"] . ", '" . createTimeStamp($_POST["input_from"]) . "',
- '" . createTimeStamp($_POST["input_to"]) . "', " . $_POST["input_type"] . ", " . $_POST["input_distance"] . ");";
+$addQuery = $_POST["input_day"] . ", " . $_SESSION["id"] . ", " . createTimeStamp($_POST["input_from"]) . ", " . createTimeStamp($_POST["input_to"]) . ", " . $_POST["input_type"] . ", " . $_POST["input_distance"];
 
+$sqlTry = "INSERT INTO tries (query) VALUES ('" . $addQuery . "');"; 
+if (mysqli_query($link, $sqlTry)) {
+    $sqlTry = "succ";
+} else {
+    $sqlTry = "fail";
+}
+
+$sql = "INSERT INTO inputs (user_id, t_from, t_to, activity, distance) VALUES (" . $addQuery . ");";
 if (mysqli_query($link, $sql)) {
     echo "Záznam přidán do databáze";
 } else {
@@ -16,7 +22,13 @@ mysqli_close($link);
 
 function createTimeStamp($time){
     $d = str_replace('-', ',', $_POST["input_day"]);
+    $d = str_replace(' ', '', $d);
+    $d = ((strpos("-", $d) == 1) ? ("0" . $d) : $d);
+    $d = ((strpos("-", $d, 3) == 4) ? str_replace("-", "-0", $d) : $d);
+    
     $t = str_replace(':', ',', $time);
+    $t = ((strlen($t) < 5) ? ("0" . $t) : $t); 
+
     $date = $t . ', 0,' . $d;
     $fulldate = explode(',', $date);
     
@@ -25,7 +37,7 @@ function createTimeStamp($time){
     $s = $fulldate[2];
     $y = $fulldate[3];
     $m = $fulldate[4];
-    $d =$fulldate[5];
+    $d = $fulldate[5];
 
     return date("Y-m-d H:i:s", mktime($h,$i,$s,$m,$d,$y));
 }
