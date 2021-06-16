88a40aec0081401578b448677700c25c958af3e1
Vojtěch Divoký <divokyvojta@gmail.com>
2021-06-16 11:18:04 +0200

    fixnuto zapisování ze Safari (iOS)

--- scripts/addInputFromValues.php
+++ scripts/addInputFromValues.php
@@ -2,16 +2,14 @@
 session_start();
 require_once "config.php";
 
-$addQuery = $_POST["input_day"] . ", " . $_SESSION["id"] . ", " . createTimeStamp($_POST["input_from"]) . ", " . createTimeStamp($_POST["input_to"]) . ", " . $_POST["input_type"] . ", " . $_POST["input_distance"];
-
-$sqlTry = "INSERT INTO tries (query) VALUES ('" . $addQuery . "');"; 
+$sqlTry = "INSERT INTO tries (query) VALUES ('" . $_SESSION["id"] . ", " . $_POST["input_from"] . ", " . $_POST["input_to"] . ", " . $_POST["input_day"] . ", " . $_POST["input_type"] . ", " . $_POST["input_distance"] . "');"; 
 if (mysqli_query($link, $sqlTry)) {
     $sqlTry = "succ";
 } else {
     $sqlTry = "fail";
 }
 
-$sql = "INSERT INTO inputs (user_id, t_from, t_to, activity, distance) VALUES (" . $addQuery . ");";
+$sql = "INSERT INTO inputs (user_id, t_from, t_to, activity, distance) VALUES (" . $_SESSION["id"] . ", '" . createTimeStamp($_POST["input_from"]) . "', '" . createTimeStamp($_POST["input_to"]) . "', " . $_POST["input_type"] . ", " . $_POST["input_distance"] . ");";
 if (mysqli_query($link, $sql)) {
     echo "Záznam přidán do databáze";
 } else {
@@ -22,14 +20,16 @@ mysqli_close($link);
 
 function createTimeStamp($time){
     $d = str_replace('-', ',', $_POST["input_day"]);
+    $d = str_replace('. ', ',', $d);
     $d = str_replace(' ', '', $d);
-    $d = ((strpos("-", $d) == 1) ? ("0" . $d) : $d);
-    $d = ((strpos("-", $d, 3) == 4) ? str_replace("-", "-0", $d) : $d);
+
+    $d = ((strpos(",", $d) == 4) ? substr_replace($d, ",0", 4) : $d);
+    $d = ((strlen($d) < 10) ? substr_replace($d, ",0", 6) : $d);
     
     $t = str_replace(':', ',', $time);
     $t = ((strlen($t) < 5) ? ("0" . $t) : $t); 
 
-    $date = $t . ', 0,' . $d;
+    $date = $t . ', 0, ' . $d;
     $fulldate = explode(',', $date);
     
     $h = $fulldate[0];
