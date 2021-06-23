a712810c4ce3d32153b5142cfefcad19e5afc30f
Vojtěch Divoký <divokyvojta@gmail.com>
2021-06-23 11:11:57 +0200

    text input rozdělen na kousky, připraveno na tvorbu objektů

--- scripts/addInputFromText.php
+++ scripts/addInputFromText.php
@@ -1,6 +1,6 @@
 <?php
 //echo $_POST["input_text"];
-$input = "6/2021 .3) 10:50 12:30 t | 12:30 13:45 d .5) 11:50 13:40 d | 18:00 20:55 s !36!";
+$input = "6/2021 .3) 10:50 12:30 s !44! | 12:30 13:45 d .5) 11:50 13:40 d | 18:00 20:55 s !36! .16) 10:00 13:30 t";
 $input = preg_replace('/\s+/', '', $input);
 
 echo $input . "<br>";
@@ -32,20 +32,32 @@ $realInputs = [];
 for($i = 0; $i < count($inputs); $i++){
     $curr = $inputs[$i];
     
-    $day = substr($curr, 1, strpos($curr, ")"));
-    $curr = substr($curr, strpos($curr, ")")+1);
-    echo "-" . $curr . "-<br>";
+    $day = substr($curr, 1, strpos($curr, ")") - 1);
+    $curr = substr($curr, strpos($curr, ")") + 1);
     echo "d: -" . $day . "-<br>";
+    echo "-" . $curr . "-<br>";
     
-    for($j = 0; $j < substr_count($curr, ":") / 2; $j++){
+    $num = substr_count($curr, ":") / 2;
+    echo "intervals: " . $num . "<br>";
+    for($j = 0; $j < $num; $j++){
         $from = substr($curr, strpos($curr, ":") - 2, 5);
-        $curr = substr($curr, strpos($curr, ":"), 3);
-        $to = substr($curr, strpos($curr, ":") - 2, 5);
+        $to = substr($curr, strpos($curr, ":", 3) - 2, 5);
+        $a = substr($curr, strpos($curr, ":", 3) + 3, 1);
+        
         
         echo "f: -" . $from . "-<br>";
         echo "t: -" . $to . "-<br>";
+        echo "a: -" . $a . "-<br>";
+        if($a == "s"){
+            $mFirst = strpos($curr, "!") + 1;
+            $mSecond = strpos($curr, "!", $mFirst);
+            $dist = substr($curr, $mFirst, $mSecond - $mFirst);
+            echo "dist: -" . $dist . "-<br>";
+        }
+        
+        $curr = str_replace(substr($curr, 0, (($a == "s") ? $mSecond + 2 : strpos($curr,$a) + 2)), "", $curr);
+        echo "rem: -" . $curr . "-<br>";
     }
-
     echo "<br>";
 }
 
